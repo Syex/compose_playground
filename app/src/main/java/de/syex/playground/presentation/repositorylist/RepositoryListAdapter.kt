c33090fdf19951dbc14f50a61df0a6017718a4d5
Tom Seifert <syex@seifertion.de>
2020-09-05 12:00:07 +0200

    Display repos in a RecyclerView

---
+++ app/src/main/java/de/syex/playground/presentation/repositorylist/RepositoryListAdapter.kt
@@ -0,0 +1,55 @@
+package de.syex.playground.presentation.repositorylist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import de.syex.playground.R
+import de.syex.playground.domain.model.Repository
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_repository.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RepositoryListAdapter : ListAdapter<Repository, RepositoryListAdapter.ViewHolder>(
+    RepoItemCallback()
+) {
+
+    private val dateFormat = SimpleDateFormat("dd.MM.yyy", Locale.getDefault())
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return LayoutInflater.from(parent.context).inflate(R.layout.item_repository, parent, false)
+            .let { ViewHolder(it) }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val repo = getItem(position)
+        with(holder.containerView) {
+            repoName.text = repo.name
+            issueCount.text = repo.openIssuesCount.toString()
+            forkCount.text = repo.forksCount.toString()
+            licenseName.text = repo.license?.name
+            createdAtText.text = dateFormat.format(repo.createdAt)
+            updatedAtText.text = dateFormat.format(repo.updatedAt)
+        }
+    }
+
+    class ViewHolder(
+        override val containerView: View
+    ) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer
+
+}
+
+private class RepoItemCallback : DiffUtil.ItemCallback<Repository>() {
+
+    override fun areItemsTheSame(oldItem: Repository, newItem: Repository): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Repository, newItem: Repository): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/de/syex/playground/presentation/repositorylist/RepositoryListFragment.kt
+++ app/src/main/java/de/syex/playground/presentation/repositorylist/RepositoryListFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import de.syex.playground.R
+import kotlinx.android.synthetic.main.repository_list_fragment.*
+import kotlinx.android.synthetic.main.repository_list_fragment.view.*
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -18,6 +23,7 @@ import kotlinx.coroutines.flow.onEach
 class RepositoryListFragment : Fragment() {
 
     private val viewModel: RepositoryListViewModel by viewModels { RepositoryListViewModelFactory() }
+    private val repoAdapter = RepositoryListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,9 +35,27 @@ class RepositoryListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.repositoryRecyclerView.apply {
+            adapter = repoAdapter
+            addItemDecoration(DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
+        }
+
         viewModel.stateFlow
-            .onEach { println(it) }
+            .onEach { updateState(it) }
             .launchIn(lifecycleScope)
+    }
+
+    private fun updateState(state: RepositoryListState) = when (state) {
+        RepositoryListState.Loading -> progressBar.show()
+        RepositoryListState.DisplayError -> Toast.makeText(
+            requireContext(),
+            "An error occurred",
+            Toast.LENGTH_SHORT
+        ).show()
+        is RepositoryListState.DisplayRepositories -> {
+            progressBar.hide()
+            repoAdapter.submitList(state.repositories)
+        }
     }
 
     override fun onResume() {
